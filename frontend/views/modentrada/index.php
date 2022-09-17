4eefa1bb4a3674f4dceb62b23f98ada9527a3aa0
Maria Teresa <mariatst@nauta.cu>
2022-09-17 10:41:43 -0400

    Arreglando bugs en cruds modsalida, modentrada, trabajadores y estrabajo

--- frontend/views/modentrada/index.php
+++ frontend/views/modentrada/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
-use \frontend\models\Modentrada;
+use app\models\Modentrada;
 
 /* @var $this yii\web\View */
 /* @var $searchModel frontend\models\ModentradaSearch */
